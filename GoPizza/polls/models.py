15db83072ea98cea6848de013e8b3af2c2107924
ivanambroziak <ivan.v.ambroziak@ukd.edu.ua>
2025-03-24 11:09:42 +0200

    Created question and choice classes

--- GoPizza/polls/models.py
+++ GoPizza/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length = 250)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+class Choice(models.Model):
+    question = models.CharField(max_length = 250)
+    choice_text = models.TextField()
+    votes = models.IntegerField()
