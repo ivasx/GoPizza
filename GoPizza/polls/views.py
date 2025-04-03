b6615649b591f019a494cb3e14c63470ecb16912
ivanambroziak <ivan.v.ambroziak@ukd.edu.ua>
2025-04-03 09:48:30 +0300

    Create question_list.html

--- GoPizza/polls/views.py
+++ GoPizza/polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from polls.models import Question
+
 
 # Create your views here.
+def question_list(request):
+    objects = Question.objects.all()
+    context = {'objects': objects}
+    return render(request, 'question_list.html', context)
