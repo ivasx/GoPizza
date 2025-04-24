0b93ba335ebd9b865e29ace734614ca93889d537
ivasx <ambroziak.v.ivan@gmail.com>
2025-04-24 09:08:05 +0300

    Update urls.py

--- GoPizza/polls/urls.py
+++ GoPizza/polls/urls.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
+from .views import login_view
 
 app_name = 'polls'
-urlpatterns = [
-    path('', views.question_list, name='question_list'),
-    path('accounts/', include('django.contrib.auth.urls')),
-]
 
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('questions/', views.question_list, name='question_list'),
+    path('register/', views.register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
