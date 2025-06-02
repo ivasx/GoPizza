bb4d735ff3f68b206d7dafaa0548fca81a7171c4
ivasx <ambroziak.v.ivan@gmail.com>
2025-06-02 14:48:18 +0300

    Update settings.py

--- GoPizza/GoPizza/settings.py
+++ GoPizza/GoPizza/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1234567890abcdefghijklmnopqrstuvwxyz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Application definition
